6ef3bdd489ffd7886d7246d0515ab3def9000fc2
Gratian Lup <gratianlup@outlook.com>
2013-01-23 15:32:07 +0200

    Update SmartFlip/OverlayWindow.xaml.cs

--- SmartFlip/OverlayWindow.xaml.cs
+++ SmartFlip/OverlayWindow.xaml.cs
@@ -1,14 +1,24 @@
-﻿// Copyright (c) Gratian Lup. All rights reserved.
+// Copyright (c) Gratian Lup. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// Copyright (c) Gratian Lup. All rights reserved.
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
 //
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above
-//       copyright notice, this list of conditions and the following
-//       disclaimer in the documentation and/or other materials provided
-//       with the distribution.
+// * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following
+// disclaimer in the documentation and/or other materials provided
+// with the distribution.
+//
+// * The name "SmartFlip" must not be used to endorse or promote
+// products derived from this software without prior written permission.
+//
+// * Products derived from this software may not be called "SmartFlip" nor
+// may "SmartFlip" appear in their names without prior written
+// permission of the author.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -129,7 +139,6 @@ namespace SmartFlip {
 
         public void startFadeOutAnimation(int n, int duration) {
             duration = 0;
-            // don't use animation
             titles[n].title.Opacity = 0;
         }
 
@@ -181,8 +190,7 @@ namespace SmartFlip {
             margins.cxRightWidth = Convert.ToInt32(-1 * (DesktopDpiX / 96));
             margins.cyTopHeight = Convert.ToInt32(-1 * (DesktopDpiX / 96));
             margins.cyBottomHeight = Convert.ToInt32(this.ActualHeight * (DesktopDpiX / 96));
-
-            int hr = DwmExtendFrameIntoClientArea(mainWindowSrc.Handle, ref margins);
+            DwmExtendFrameIntoClientArea(mainWindowSrc.Handle, ref margins);
         }
 
     }
